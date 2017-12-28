5cdd8a0e4134575842d0d086122217e21b13ca9d
Nick Atkins <littledumbco@gmail.com>
2017-12-28 10:26:38 +0000

    Button Logic
    So, using the LevelManager script I've now got it hooked up so you can neatly jump between the different scenes.
    
    Potentially thinking about branching out on the text-adventure game now to include this, as I'll then potentially be able to use a combo of states and scenes to make something really cool.
    
    Note: Added scenes to the scene list in build settings - important as otherwise it doesn't recognise the scenes.

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
+	// Creating a basic function to handle loading levels
 	public void LoadLevel(string name){
 		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel (name);
 	}
 
+	// Creating another basic function for handling quitting the game
 	public void QuitRequest (){
 		Debug.Log ("Quit requested");
+		Application.Quit ();
 	}
 
 }
