6ed4fccbfeea3accf45b7cd795e093a5633a12f1
Nick Atkins <littledumbco@gmail.com>
2017-12-30 10:27:06 +0000

    NumberWizard Imported
    So, after ensuring the scenes could happily jump between each other and had the logic in place, I had to import in the Number Wizard script from the first tutorial.
    
    A lot of it was now redundant, such as the printing to console, so I stripped it back and introduced a couple of public functions that enabled me to add them to the physical UI buttons within the scene.
    
    Then also added in the ability to print the computer's guess to a string of UI text that's also in the scene.
    
    That's pretty much the whole game done from a purely functional stand point. Next up will be to tidy it up into something a bit more presentable!

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	
+	// creating a couple of number variables
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 10;
+
+	public Text guessText;
+	
+	// Use this for initialization
+	void Start () {
+		
+		// start the game function
+		StartGame ();
+	}
+	
+	void StartGame () {
+		// setting the value of the variables
+		max = 1000;
+		min = 1;
+		guess = 500;
+		// fixes the rounding problem of stopping at 999
+		max = max +1;
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower (){
+		max = guess;
+		NextGuess ();
+	}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		guessText.text = guess.ToString ();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
